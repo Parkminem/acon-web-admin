14965e0c96a032b0c35de23569405d20ef3c19e4
Park Sohee <pssh5124@gmail.com>
2022-12-19 17:55:44 +0900

    chore : 개발환경 세팅

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <router-view></router-view>
+</template>
+<script setup></script>
+<style>
+@import 'reset-css';
+@import url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap');
+body {
+  font-family: 'Noto Sans KR', sans-serif;
+}
+</style>
---
+++ src/routes/index.js
@@ -0,0 +1,91 @@
+import { createWebHistory, createRouter } from 'vue-router';
+
+const routes = [
+  {
+    path: '/main',
+    name: 'defaultLayout',
+    component: () => import('../layout/DefaultLayout.vue'),
+    children: [
+      {
+        path: '',
+        component: () => import('../views/MainView.vue')
+      },
+      {
+        path: '/history',
+        component: () => import('../views/HistoryView.vue')
+      },
+      {
+        path: '/news',
+        component: () => import('../views/NewsView.vue')
+      },
+      {
+        path: '/partners',
+        component: () => import('../views/PartnersView.vue')
+      },
+      {
+        path: '/location',
+        component: () => import('../views/LocationView.vue')
+      },
+      {
+        path: '/questionlist',
+        component: () => import('../views/Question/questionListView.vue')
+      },
+      {
+        path: '/question',
+        component: () => import('../views/question/QuestionView.vue')
+      },
+      {
+        path: '/promotion',
+        component: () => import('../views/PromotionView.vue')
+      },
+      {
+        path: '/employee',
+        component: () => import('../views/management/EmployeeView.vue')
+      },
+      {
+        path: '/computer',
+        component: () => import('../views/management/ComputerView.vue')
+      },
+      {
+        path: '/monitor',
+        component: () => import('../views/management/MonitorView.vue')
+      },
+      {
+        path: '/peripheral',
+        component: () => import('../views/management/PeripheralsView.vue')
+      },
+      {
+        path: '/book',
+        component: () => import('../views/management/BookView.vue')
+      },
+      {
+        path: '/program',
+        component: () => import('../views/management/ProgramView.vue')
+      },
+      {
+        path: '/organization',
+        component: () => import('../views/OrganizationView.vue')
+      },
+      {
+        path: '/cooperation',
+        component: () => import('../views/CooperationView.vue')
+      },
+      {
+        path: '/toonflix',
+        component: () => import('../views/ToonflixView.vue')
+      }
+    ]
+  },
+  {
+    path: '/',
+    name: 'loginLayout',
+    component: () => import('../layout/LoginLayout.vue')
+  }
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+});
+
+export default router;
