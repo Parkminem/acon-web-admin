e90a55dcfedd7427ad527d6672911257c866b84f
Park Sohee <pssh5124@gmail.com>
2023-01-12 10:15:15 +0900

    feat : 로딩바 구현

--- src/components/utils/Loading.vue
+++ src/components/utils/Loading.vue
@@ -1,15 +1,14 @@
 <template>
-  <div class="loadingBox container">
-    <div class="section">
-      <div class="imgBox">
-        <img src="/images/common/loading.gif" alt="loading" />
-      </div>
+  <div class="loadingBox">
+    <div class="imgBox">
+      <img src="/images/common/loading.gif" alt="loading" />
     </div>
   </div>
 </template>
 <script setup></script>
 <style lang="scss" scoped>
-.section {
+.loadingBox {
+  min-height: calc(100vh - 74px);
   display: flex;
   align-items: center;
   justify-content: center;
