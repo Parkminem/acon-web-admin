f62692f932ac48425fa4b88c50afa17bbaf725f9
Park Sohee <pssh5124@gmail.com>
2023-02-07 13:57:24 +0900

    build 경로 변경

--- vite.config.js
+++ vite.config.js
@@ -16,5 +16,23 @@ export default defineConfig({
         additionalData: `@import "@/assets/manager/style/_vars.scss"; @import "@/assets/manager/style/_mixin.scss";`
       }
     }
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          var info = assetInfo.name.split('.');
+          var extType = info[info.length - 1];
+          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+            extType = 'img';
+          } else if (/woff|woff2/.test(extType)) {
+            extType = 'css';
+          }
+          return `static/admin/${extType}/[name]-[hash][extname]`;
+        },
+        chunkFileNames: 'static/admin/js/[name]-[hash].js',
+        entryFileNames: 'static/admin/js/[name]-[hash].js'
+      }
+    }
   }
 });
