85b7d6b3ac2f3241bf5aaad3e9d1894a5c1283bd
박소희 <pssh5124@gmail.com>
2023-03-02 11:49:18 +0900

    로딩이미지 경로 수정

--- src/components/utils/Loading.vue
+++ src/components/utils/Loading.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="loading-box">
     <div class="imgBox">
-      <img src="@/assets/manager/images/common/loading.gif" alt="loading" />
+      <img :src="loadingImg" alt="loading" />
     </div>
   </div>
 </template>
-<script setup></script>
+<script setup>
+import loadingImg from '@/assets/manager/images/common/loading.gif';
+</script>
 <style lang="scss" scoped>
 .loading-box {
   min-height: calc(100vh - 74px);
